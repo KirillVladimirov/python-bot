4712072ccbe2d9e623234e4c52a8d9475aabdb6b
Кирилл Владимиров <kvladimirov.main@gmail.com>
2018-04-07 19:31:39 +0300

    aaa

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 import telebot
 import requests
 import datetime
 import os
+import logging
+from aiohttp import web
 
-config = []
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
+config = dict()
+
 config['API_TOKEN'] = os.environ.get('API_TOKEN', None)
 config['WEBHOOK_URL_BASE'] = os.environ.get('WEBHOOK_URL_BASE', None)
 config['WEBHOOK_URL_PATH'] = "/%s/".format(config['API_TOKEN'])
+config['WEBHOOK_LISTEN'] = '0.0.0.0'
+config['WEBHOOK_PORT'] = 8443
 
 bot = telebot.TeleBot(config['API_TOKEN'])
+bot.remove_webhook()
+bot.set_webhook(url=config['WEBHOOK_URL_BASE'])
 
 
-# bot.remove_webhook()
-# bot.set_webhook(url=config['WEBHOOK_URL_BASE'] + config['WEBHOOK_URL_PATH'])
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+
+app = web.Application()
+app.router.add_post('/{token}/', handle)
+
+# web.run_app(
+#     app,
+#     host=config['WEBHOOK_LISTEN'],
+#     port=config['WEBHOOK_PORT'],
+# )
---
+++ conf.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['API_TOKEN'] = '591002265:AAGEDlHfVeUtPVNLvBKFDAHQpgLccLeNb4M'
+os.environ['WEBHOOK_URL_BASE'] = 'https://boiling-wave-41860.herokuapp.com'
