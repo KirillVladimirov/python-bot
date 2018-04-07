612f8b9dfc2293cced043137fa0eacb03b6c95a7
Кирилл Владимиров <kvladimirov.main@gmail.com>
2018-04-07 16:31:39 +0300

    9

--- app.py
+++ app.py
@@ -13,40 +13,25 @@ bot = telebot.TeleBot(app.config['API_TOKEN'])
 
 app = Flask(__name__)
 
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
 
-# Set webhook
-bot.set_webhook(url=app.config['WEBHOOK_URL_BASE'] + app.config['WEBHOOK_URL_PATH'])
-
-
-# Empty webserver index, return nothing, just http 200
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-# Process webhook calls
-@app.route(app.config['webhook_url_path'], methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-                  "I am here to echo your kind words back to you."))
-
-
-# Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+@app.route("/токен бота", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://ссылка на приложение/токен вашего бота")
+    return "!", 200
