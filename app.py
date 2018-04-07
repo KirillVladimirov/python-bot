ae6f404f60d4a1d44c457d66e0a5a1662ab59b5a
Кирилл Владимиров <kvladimirov.main@gmail.com>
2018-04-07 18:32:21 +0300

    11

--- app.py
+++ app.py
@@ -11,74 +11,14 @@ config['WEBHOOK_URL_PATH'] = "/%s/".format(config['API_TOKEN'])
 
 bot = telebot.TeleBot(config['API_TOKEN'])
 
-bot.remove_webhook()
-bot.set_webhook(url=config['WEBHOOK_URL_BASE'] + config['WEBHOOK_URL_PATH'])
 
+# bot.remove_webhook()
+# bot.set_webhook(url=config['WEBHOOK_URL_BASE'] + config['WEBHOOK_URL_PATH'])
 
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(config['API_TOKEN'])
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-
-
-greet_bot = BotHandler(config['API_TOKEN'])
-greetings = ('hello', 'hi', 'greetings', 'sup')
-now = datetime.datetime.now()
-
-
-def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
-
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
-    main()
+    bot.polling(none_stop=True)
